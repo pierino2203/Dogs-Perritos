20af33b26390c0eebeeb42c077668f7f96fd30e4
Pierino esteban Juncos <pierinojuncos@MacBook-Pro-de-Pierino.local>
2024-07-16 16:08:01 -0300

    Subiendo-DployFront5

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 module.exports = function override(config, env) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    "path": require.resolve("path-browserify")
+    "path": require.resolve("path-browserify"),
+    "os": require.resolve("os-browserify/browser")
   };
   return config;
 };
-

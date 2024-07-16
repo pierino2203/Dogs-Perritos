962fd8ee448e91685761e508d7f78a2da75d2ad8
Pierino esteban Juncos <pierinojuncos@MacBook-Pro-de-Pierino.local>
2024-07-16 16:01:19 -0300

    Subiendo-DployFront3

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,19 +2,8 @@ const webpack = require('webpack');
 
 module.exports = function override(config, env) {
   config.resolve.fallback = {
-    "buffer": require.resolve("buffer/"),
-    "path": require.resolve("path-browserify"),
-    "os": require.resolve("os-browserify/browser"),
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "process": require.resolve("process/browser")
+    ...config.resolve.fallback,
+    "vm": require.resolve("vm-browserify")
   };
-
-  config.plugins = (config.plugins || []).concat([
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-  ]);
-
   return config;
 };
